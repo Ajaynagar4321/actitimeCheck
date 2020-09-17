580c87a2e51c8b153c8d0d6ae9b29c17d71ebfd4
Girish <Girish@DESKTOP-HSLV41C>
2020-09-17 12:53:05 +0530

    New one

---
+++ actitime/src/test/java/com/actitime/test/actitime/Te.java
@@ -0,0 +1,13 @@
+package com.actitime.test.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Te {
+
+	@Test
+	void ru()
+	{
+		Reporter.log("dfds");
+	}
+}
