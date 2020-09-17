eb9fc6ccaa07a4311cecfbad734def12b0dd6105
Girish <Girish@DESKTOP-HSLV41C>
2020-09-17 11:27:16 +0530

    My new Project

---
+++ actitime/src/main/java/com/actitime/test/actitime/App.java
@@ -0,0 +1,10 @@
+package com.actitime.test.actitime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+  
+}
---
+++ actitime/src/test/java/com/actitime/test/actitime/TestDemo.java
@@ -0,0 +1,15 @@
+package com.actitime.test.actitime;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+	@Test
+	public void testDemo()
+	{
+		System.out.println("test check");
+	}
+	
+	
+	
+}
