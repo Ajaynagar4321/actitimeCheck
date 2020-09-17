73bf39bdc99e0c3e8dfed06b4d0326ba2d6a193a
Girish <Girish@DESKTOP-HSLV41C>
2020-09-17 13:23:15 +0530

    new rr

---
+++ actitime/src/test/java/com/actitime/test/actitime/NWEW.java
@@ -0,0 +1,12 @@
+package com.actitime.test.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NWEW {
+@Test
+void fff()
+{
+	Reporter.log("rrr",true);
+}
+}
